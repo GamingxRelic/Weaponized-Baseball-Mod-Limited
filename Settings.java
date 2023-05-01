0efa07c23c7414d3d2f5bc7c19933b4ac6a37b3d
Relic <86666549+GamingxRelic@users.noreply.github.com>
2023-05-01 13:55:48 -0400

    Changes to mod

---
+++ Settings.java
@@ -0,0 +1,9 @@
+package blizzardfenix.webasemod.util;
+
+public class Settings {
+	public static boolean overrideBounciness = false;
+	public static boolean overrideFriction = false;
+	public static float bounciness;
+	public static float friction;
+	public static Boolean throwUp;
+}
